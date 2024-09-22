8bfcb5622c691906de339b00f34976eade57448d
yangxiaoping <2878847069@qq.com>
2024-09-22 19:05:05 +0800

    feat: 工厂模式

---
+++ packages/1.design-pattern/1.creation-pattern/1-1.factory.js
@@ -0,0 +1,36 @@
+/*
+工厂模式，也可以叫简单工厂模式
+*/
+// 定义产品类
+class Book {
+  constructor (title, author) {
+    this.title = title
+    this.author = author
+  }
+  getTitle () {
+    return this.title
+  }
+}
+
+class Pen {
+  constructor (color) {
+    this.color = color
+  }
+}
+
+// 定义一个工厂函数 分发创建产品
+class Factory {
+  create (type) {
+    switch (type) {
+      case 'book':
+        return new Book('Harry Potter', 'J.K. Rowling')
+      case 'pen':
+        return new Pen('red')
+      default:
+        throw new Error(`Unknown type: ${type}`)
+    }
+  }
+}
+
+console.log(new Factory().create('book').getTitle())
+console.log(new Factory().create('pen').color)
---
+++ packages/1.design-pattern/docs/README.md
@@ -0,0 +1,26 @@
+
+# Design Patterns
+
+- [JavaScript设计模式es6（23种)](https://juejin.cn/post/6844904032826294286)
+- [10分钟掌握JavaScript设计模式](https://juejin.cn/post/7052148234097000462)
+
+# 1.Table of Contents
+
+## 1-1.创建型模式
+
+### 1-1-1.工厂模式
+
+[工厂模式](../1.creation-pattern/1-1.factory.js)
+
+### 1-1-2.抽象工厂模式
+
+### 1-1-3.单例模式
+
+### 1-1-4.构造器模式
+
+### 1-1-5.原型模式
+
+## 1-2.结构型模式
+
+## 1-3.行为型模式
+
